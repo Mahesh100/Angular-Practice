225d8bde8b5718546f509a5c808be86490d89c7d
Mahesh Kakde <maheshkakde100@gmail.com>
2024-04-21 07:53:57 +0530

    added

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignInComponent } from './user_auth/sign-in/sign-in.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    SignInComponent,
+    UserListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
